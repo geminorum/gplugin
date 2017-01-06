ba1bf0e93d691ef0f7b0b271cbe293f8d1c42af7
Nasser Rafie <contact@geminorum.ir>
2017-01-06 18:37:51 +0330

    wphelper: static caching slug lookups

--- includes/wphelper.class.php
+++ includes/wphelper.class.php
@@ -172,28 +172,35 @@ if ( ! class_exists( 'gPluginWPHelper' ) ) { class gPluginWPHelper extends gPlug
 	// FROM: gEditorialHelper
 	public static function getPostIDbySlug( $slug, $post_type, $url = FALSE )
 	{
-		global $wpdb;
+		static $strings = array();
 
 		if ( $url ) {
 			$slug = rawurlencode( urldecode( $slug ) );
 			$slug = sanitize_title( basename( $slug ) );
 		}
 
+		$slug = trim( $slug );
+
+		if ( isset( $strings[$post_type][$slug] ) )
+			return $strings[$post_type][$slug];
+
+		global $wpdb;
+
 		$post_id = $wpdb->get_var(
 			$wpdb->prepare(
 				"SELECT ID FROM $wpdb->posts WHERE post_name = %s AND post_type = %s",
-				trim( $slug ),
+				$slug,
 				$post_type
 			)
 		);
 
 		if ( is_array( $post_id ) )
-			return $post_id[0];
+			return $strings[$post_type][$slug] = $post_id[0];
 
 		else if ( ! empty( $post_id ) )
 			return $post_id;
 
-		return FALSE;
+		return $strings[$post_type][$slug] = FALSE;
 	}
 
 	public static function getCurrentPostType()
